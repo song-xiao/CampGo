c829af5c52edf0b9c8eacdd75bb58b1cdc89a958
Song <christiexs@gmail.com>
2018-08-09 19:43:52 +0000

    Customize images to take up 100% of the space provided.

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ mongoose.connect("mongodb://localhost/go_camp");
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
+app.use(express.static(__dirname + "/public"));
 
 app.get("/",function(req,res){
    res.render("landing"); 
@@ -127,10 +128,6 @@ app.post("/campgrounds/:id/comments",function(req,res){
             });
         }
     });
-    
-    
-    // redirect campground show page
-   
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){

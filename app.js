81c25349b72774a8458297fdc5bc7d4955ba81d4
Song <christiexs@gmail.com>
2018-08-09 00:15:21 +0000

    Connect the app to the Database: MongoDB.

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express = require("express"), 
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/go_camp");
+
+// setup scheme
+var campgroundScheme = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+// setup model
+var Campground = mongoose.model("Campground",campgroundScheme);
+    
+// Campground.create(
+//     { name: "Narin Falls", image: "https://cdn.pixabay.com/photo/2016/11/21/15/14/camping-1845906_1280.jpg"
+//     },function(err,camp){
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log("Newly created camp");
+//             console.log(camp);
+//         }
+//     }
+// );
+
 // Arbitrary campgrounds
-var campgrounds = [
-    { name: "Porteau Cove", image: "https://cdn.pixabay.com/photo/2014/11/27/18/36/tent-548022_1280.jpg"},
-    { name: "Narin Falls", image: "https://cdn.pixabay.com/photo/2016/11/21/15/14/camping-1845906_1280.jpg"},
-    { name: "Cultus Lake", image: "https://cdn.pixabay.com/photo/2016/11/29/04/17/bonfire-1867275_1280.jpg"},
-    { name: "Alice Lake", image: "https://cdn.pixabay.com/photo/2016/11/22/23/08/adventure-1851092_1280.jpg"}
-    ];
+// var campgrounds = [
+//     { name: "Porteau Cove", image: "https://cdn.pixabay.com/photo/2014/11/27/18/36/tent-548022_1280.jpg"},
+//     { name: "Narin Falls", image: "https://cdn.pixabay.com/photo/2016/11/21/15/14/camping-1845906_1280.jpg"},
+//     { name: "Cultus Lake", image: "https://cdn.pixabay.com/photo/2016/11/29/04/17/bonfire-1867275_1280.jpg"},
+//     { name: "Alice Lake", image: "https://cdn.pixabay.com/photo/2016/11/22/23/08/adventure-1851092_1280.jpg"}
+//     ];
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
@@ -17,7 +42,14 @@ app.get("/",function(req,res){
 });
 
 app.get("/campgrounds",function(req,res){
-    res.render("campgrounds", {campgrounds:campgrounds})
+    // get all campgrounds from DB
+    Campground.find({},function(err,camps){
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("campgrounds", {campgrounds:camps})
+        }
+    });
 });
 
 app.post("/campgrounds",function(req,res){
@@ -28,9 +60,16 @@ app.post("/campgrounds",function(req,res){
         name: name,
         image: image
     };
-    campgrounds.push(newCamp);
-    // redirect back to campground page
-    res.redirect("/campgrounds");
+    // create a new campground and save to DB
+    Campground.create(newCamp, function(err,camp){
+            if (err){
+                console.log(err);
+            } else {
+                // redirect back to campground page
+                res.redirect("/campgrounds");
+            }
+        }
+    );
 });
 
 app.get("/campgrounds/new",function(req, res){

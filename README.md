ed2ec178e73741fd9e7ceec3d974f85abccfd10e
Song <christiexs@gmail.com>
2018-08-10 23:56:30 +0000

    Refactor app.js and update readme file.

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # GoCamp
 
-A web app providing campsite information and making reservation.
+A simple web application to create and check out campgrounds. It can be accessed at: https://secure-dawn-50161.herokuapp.com/
+
+All users can view the campgrounds and comments. To create campground or publish comments, user must sign up and log in.
+
+Users can create a campground by providing its name, price, image and description. Only those who create a campground can edit or delete it. 
+
+Users can always edit or delete his/her comments. Flash messages are used to provide feedbacks.
+
--- app.js
+++ app.js
@@ -1,27 +1,28 @@
-var express     = require("express"),
-    app         = express(),
-    bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose"),
-    passport    = require("passport"),
-    LocalStrategy = require("passport-local"),
-    methodOverride = require("method-override"),
-    flash = require("connect-flash"),
-    User        = require("./models/user");
+var express         = require("express"),
+    app             = express(),
+    bodyParser      = require("body-parser"),
+    mongoose        = require("mongoose"),
+    passport        = require("passport"),
+    LocalStrategy   = require("passport-local"),
+    methodOverride  = require("method-override"),
+    flash           = require("connect-flash"),
+    User            = require("./models/user");
     
-//requring routes
+// requring routes
 var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index");
     
-// mongoose.connect("mongodb://localhost/go_camp");
+// connect to mLab database
 mongoose.connect("mongodb://song:gocampv1@ds119422.mlab.com:19422/gocamp");
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
 
-// PASSPORT CONFIGURATION
+// passport configuration
 app.use(require("express-session")({
     secret: "Once again Rusty wins cutest dog!",
     resave: false,
@@ -33,9 +34,11 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+// setup local variables
 app.use(function(req, res, next){
    res.locals.currentUser = req.user;
-   res.locals.error = req.flash("error");           // local variable for flash message
+   // local variables for flash message
+   res.locals.error = req.flash("error"); 
    res.locals.success = req.flash("success"); 
    next();
 });
@@ -45,7 +48,6 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-
 app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("app starts!");
+    console.log("App starts!");
 });
